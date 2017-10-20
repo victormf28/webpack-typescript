914727aa0285d839ab7d6d1eec045cbdb029f413
Victor Morales <victor_flores_5@hotmail.com>
2017-10-20 00:27:20 -0500

    Feat: Creando estructura de carpetas

---
+++ source/modules/ficha/all/slider.js
@@ -0,0 +1,29 @@
+export default (params)=>{
+    let st = {},
+    dom = {},
+    defaults,
+    catchDom,
+    fn
+
+    defaults = {
+        parent: ".parent",
+        ele: ".ele"
+    }
+
+    catchDom = () =>{
+        dom.parent = $(st.parent)
+        dom.ele = $(st.ele, dom.parent)
+    }
+
+    fn = {
+        setup(){
+            console.log("Loading Slider ...")
+        }
+    }
+
+    (() => {
+        st = $.extend({}, defaults, params)
+        catchDom()
+        fn.setup()
+    })()
+}
